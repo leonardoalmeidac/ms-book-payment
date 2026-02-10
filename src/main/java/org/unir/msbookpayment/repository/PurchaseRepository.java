214016be4de966cafab284944dfa9622e15559e6
Leonardo Almeida <leonardo.almeidac@gmail.com>
2026-02-09 21:49:15 -0500

    feat: add new configs

--- src/main/java/org/unir/msbookpayment/repository/PurchaseRepository.java
+++ src/main/java/org/unir/msbookpayment/repository/PurchaseRepository.java
@@ -3,7 +3,5 @@ package org.unir.msbookpayment.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.unir.msbookpayment.model.Purchase;
 
-;
-
 public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
 }
